f17848987adc262de47a521a12fb5b1c6dbfe500
jakemal <maldonadojm@s.dcsdk12.org>
2015-10-21 09:53:44 -0600

    Stable Version 1.0 - Need to add Score

---
+++ Player.rb
@@ -0,0 +1,65 @@
+require "Gosu"
+require_relative "z_order"
+
+class Player
+
+	TURN_INCREMENT = 5.5
+	ACCELERATION = 0.5
+	COLLISION_DISTANCE = 30
+
+	def initialize
+		@x = @y = @vel_x = @vel_y = @angle = 0.0
+		@score = 0
+		@image = Gosu::Image.new("media/asian.jpg")
+	end
+
+	def warp(x, y)
+		@x, @y = x, y
+	end
+
+	def turn_left
+		@angle -= TURN_INCREMENT
+	end
+
+	def turn_right
+		@angle += TURN_INCREMENT
+	end
+
+	def accelerate
+		@vel_x += Gosu::offset_x(@angle, ACCELERATION)
+		@vel_y += Gosu::offset_y(@angle, ACCELERATION)
+	end
+
+	def move
+		@x += @vel_x
+		@y += @vel_y
+
+		@x %= 640
+		@y %= 480
+
+		@vel_x *= 0.95
+		@vel_y *= 0.95	
+	end
+
+	def draw
+		@image.draw_rot(@x, @y, ZOrder::PLAYER, @angle)
+	end	
+
+	def score
+		@score
+	end
+
+
+	def collect_stars(stars)
+		if stars.reject! {|star| Gosu::distance(@x, @y, star.x, star.y) < COLLISION_DISTANCE}
+			@score += 1
+		end
+	end
+
+	private
+
+	def colliding?(star)
+		Gosu::distance(@x, @y, star.x, star.y) < COLLISION_DISTANCE
+	end
+
+end
--- main.rb
+++ main.rb
@@ -1,4 +1,7 @@
-require "gosu"
+require "Gosu"
+require_relative 'player'
+require_relative 'z_order'
+require_relative 'stars'
 
 class GameWindow < Gosu::Window
 
@@ -8,18 +11,37 @@ class GameWindow < Gosu::Window
 
 		@background_image = Gosu::Image.new("media/space.png", 
 																				:tileable => true)
+
+		@player = Player.new
+		@player.warp(width/2.0, height/2.0)
+
+		@star_anim = Gosu::Image::load_tiles("media/star.png", 25, 25)
+		@stars = Array.new
+		@font = Gosu::Font.new(20)
 	end
 
 	def update
-		
+		@player.turn_left if Gosu::button_down? Gosu::KbLeft
+		@player.turn_right if Gosu::button_down? Gosu::KbRight
+		@player.accelerate if Gosu::button_down? Gosu::KbUp
+
+		@player.move
+		@player.collect_stars(@stars)
+
+		if rand(25) < 20 && @stars.size < 50
+			@stars.push(Star.new(@star_anim))
+		end
 	end
 
 	def draw
-		@background_image.draw(0, 0, 0)
+		@player.draw
+		@background_image.draw(0, 0, ZOrder::BACKGROUND)
+		@stars.each { |star| star.draw }
+		@font.draw("Score: #{@player.score}", 10, 10, ZOrder::UI, 1.0, 1.0, 0xff_ffff00)
 	end
 
 	def button_down(id)
-		close if id == Gosu::kbEscape
+		close if id == Gosu::KbEscape
 		
 	end
 end
